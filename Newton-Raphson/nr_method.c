b78b7fa144a087f6fd4a2f5cf3c805688dbffd5b
Sachin Aditya Ramesh <71248175+batman-chemist@users.noreply.github.com>
2022-01-15 16:28:34 +0530

    Update nr_method.c

--- Newton-Raphson/nr_method.c
+++ Newton-Raphson/nr_method.c
@@ -8,8 +8,7 @@ should be defined as well.*/
 #include<math.h>
 #define f(x) 3 * x - sin(x) - 1 //define the function of interest here
 #define g(x) 3 - cos(x)       //derivative of the fucntion defined above. Change accordingly. 
-int main()
-{
+int main(){
 	float x0,x1,f0,f1,g0,g1,e=0.000001;
 	int i=1, N=0;
 	printf("\nEnter the initial guess value for the root (x0)=  ");
@@ -20,19 +19,15 @@ int main()
 	//begining of the NR method
 	printf("\n\n******************************\n\tNewton Raphson\n******************************\n\n");
 
-	do
-	{
+	do{
 	f0 = f(x0);
 	g0 = g(x0);
-
 	printf("\n\nf0= %f and g0= %f",f0,g0);
-	
-	if (g0 == 0)
-		{
+		
+	if (g0 == 0){
 		printf("\n\nMathematical error!!!\n\n");
 		exit(0);
 		}
-
 	printf("\n\nIteration number: %d",i);
 	i=i+1;
 	
@@ -44,19 +39,16 @@ int main()
 	printf("\t x1 = %f \t f1= %f",x1,f1);
 
 	x0=x1;
-	
 	}while (fabs(f1)>e && i<=N);
 
-	if (fabs(f1) > e)
-	{
+	if (fabs(f1) > e){
 		printf("\n%f\n",fabs(f1));
 		printf("\n\nNot converged\n\n");
 	}
-	else 
-	{
+	else {
 	printf("\n\nHurray!! The caclualtion has converged\n\n");
 	printf("The root of the equation is: %f\n\n",x1);
 	}
-
 	return 0;
 }
+
